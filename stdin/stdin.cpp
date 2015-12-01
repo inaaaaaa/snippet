e1ad5bc3a96dc5fa3a89f245b18a1faee9788ec7
inazo <inaba1115@gmail.com>
2015-12-01 23:45:46 +0900

    Add stdin

---
+++ stdin/stdin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/**
+ * Usage
+ * $ echo foo | ./a.out
+ */
+
+using namespace std;
+
+int main() {
+  string input;
+  cin >> input;
+  cout << "Your input is " << input << endl;
+  return 0;
+}
