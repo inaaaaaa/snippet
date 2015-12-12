c3c1d9dbdbe1ec3fa5ee90eb5d891ee09137e9b5
inazo <inaba1115@gmail.com>
2015-12-12 23:56:52 +0900

    Add lang

--- cpp/lang/reference_pointer.cpp
+++ cpp/lang/reference_pointer.cpp
@@ -3,22 +3,28 @@
 
 using namespace std;
 
+// Like Java
 void ref_func(vector<int> &v) {
   v.push_back(11);
-  v.push_back(22);
 }
 
+// C style
 void ptr_func(vector<int> *v) {
-  (*v).push_back(33);
-  (*v).push_back(44);
-  v->push_back(55);
-  v->push_back(66);
+  (*v).push_back(22);
+  v->push_back(33);
+}
+
+// This function don't change vector
+void _func(vector<int> v) {
+  v.push_back(44);
 }
 
 int main() {
   vector<int> v;
   ref_func(v);
   ptr_func(&v);
+  _func(v);
+  // v = [11, 22, 33]
   for(int i = 0; i < v.size(); ++i) {
     cout << v[i] << endl;
   }
