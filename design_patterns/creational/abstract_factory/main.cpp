9f59bf08b44f09f3bc1c682972ff609d8e629414
inazo <inaba1115@gmail.com>
2015-12-27 17:39:39 +0900

    Refactoring

--- design_patterns/creational/abstract_factory/main.cpp
+++ design_patterns/creational/abstract_factory/main.cpp
@@ -6,78 +6,78 @@ using namespace std;
 class AbstractFactory
 {
 public:
-  virtual string MakeBase() = 0;
-  virtual string MakeHeader(string title) = 0;
-  virtual string MakeHeading1(string s) = 0;
-  virtual string MakeHeading2(string s) = 0;
-  virtual string MakeParagraph(string s) = 0;
-  virtual string MakeFooter() = 0;
+  virtual string GenBase() = 0;
+  virtual string GenHeader(string title) = 0;
+  virtual string GenHeading1(string s) = 0;
+  virtual string GenHeading2(string s) = 0;
+  virtual string GenParagraph(string s) = 0;
+  virtual string GenFooter() = 0;
 };
 
 class HTMLFactory : public AbstractFactory
 {
 public:
-  string MakeBase() {
+  string GenBase() {
     return "";
   }
-  string MakeHeader(string title) {
+  string GenHeader(string title) {
     return "<!DOCTYPE html>\n<html>\n<head><title>" + title + "</title></head>\n<body>\n";
   }
-  string MakeHeading1(string s) {
+  string GenHeading1(string s) {
     return "<h1>" + s + "</h1>\n";
   }
-  string MakeHeading2(string s) {
+  string GenHeading2(string s) {
     return "<h2>" + s + "</h2>\n";
   }
-  string MakeParagraph(string s) {
+  string GenParagraph(string s) {
     return "<p>" + s + "</p>\n";
   }
-  string MakeFooter() {
-    return "</body>\n</html>\n\n";
+  string GenFooter() {
+    return "</body>\n</html>\n";
   }
 };
 
 class MarkDownFactory : public AbstractFactory
 {
 public:
-  string MakeBase() {
+  string GenBase() {
     return "";
   }
-  string MakeHeader(string title) {
+  string GenHeader(string title) {
     return "# " + title + "\n";
   }
-  string MakeHeading1(string s) {
+  string GenHeading1(string s) {
     return "## " + s + "\n";
   }
-  string MakeHeading2(string s) {
+  string GenHeading2(string s) {
     return "### " + s + "\n";
   }
-  string MakeParagraph(string s) {
+  string GenParagraph(string s) {
     return s + "  \n";
   }
-  string MakeFooter() {
-    return "\n";
+  string GenFooter() {
+    return "";
   }
 };
 
-class Document
+class Client
 {
 public:
   static string Create(AbstractFactory *factory) {
-    string s = factory->MakeBase();
-    s += factory->MakeHeader("This is Title");
-    s += factory->MakeHeading1("Yes");
-    s += factory->MakeParagraph("foo foofoo foofoofoo");
-    s += factory->MakeHeading2("No");
-    s += factory->MakeParagraph("bar barbar barbarbar");
-    s += factory->MakeFooter();
+    string s = factory->GenBase();
+    s += factory->GenHeader("This is Title");
+    s += factory->GenHeading1("Yes");
+    s += factory->GenParagraph("foo foofoo foofoofoo");
+    s += factory->GenHeading2("No");
+    s += factory->GenParagraph("bar barbar barbarbar");
+    s += factory->GenFooter();
     return s;
   }
 };
 
 int main() {
   cout << "==== start ====" << endl;
-  cout << Document::Create(new HTMLFactory);
-  cout << Document::Create(new MarkDownFactory);
+  cout << Client::Create(new HTMLFactory);
+  cout << Client::Create(new MarkDownFactory);
   cout << "==== end ====" << endl;
 }
