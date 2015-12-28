3f3ae008d129b80439496d380db58b7ceb83773a
inazo <inaba1115@gmail.com>
2015-12-29 01:05:41 +0900

    Refactoring decorator

--- design_patterns/structural/decorator/main.cpp
+++ design_patterns/structural/decorator/main.cpp
@@ -5,10 +5,9 @@ class Component
 {
 public:
   virtual std::string GetText() = 0;
-  virtual void Display() = 0;
-protected:
-  Component() {}
-  ~Component() {}
+  void Display() {
+    std::cout << this->GetText() << std::endl;
+  }
 };
 
 class ConcreteComponent : public Component
@@ -17,14 +16,9 @@ public:
   ConcreteComponent(std::string _s) {
     this->s = _s;
   }
-  ConcreteComponent() {}
-  ~ConcreteComponent() {}
   std::string GetText() {
     return this->s;
   }
-  void Display() {
-    std::cout << this->s << std::endl;
-  };
 private:
   std::string s;
 };
@@ -32,11 +26,7 @@ private:
 class Decorator : public Component
 {
 protected:
-  Decorator(Component* c) {
-    this->_component = c;
-  }
   Decorator() {}
-  ~Decorator() {}
   Component* _component;
 };
 
@@ -46,13 +36,8 @@ public:
   ConcreteDecoratorA(Component* c) {
     this->_component = c;
   }
-  ConcreteDecoratorA() {}
-  ~ConcreteDecoratorA() {}
   std::string GetText() {
     return "|" + this->_component->GetText() + "|";
-  }
-  void Display() {
-    std::cout << GetText() << std::endl;
   }
 };
 
@@ -62,25 +47,20 @@ public:
   ConcreteDecoratorB(Component* c) {
     this->_component = c;
   }
-  ConcreteDecoratorB() {}
-  ~ConcreteDecoratorB() {}
   std::string GetText() {
-    return "----" + this->_component->GetText() + "----";
-  }
-  void Display() {
-    std::cout << GetText() << std::endl;
+    return "****" + this->_component->GetText() + "****";
   }
 };
 
 int main() {
   std::cout << "==== start ====" << std::endl;
-  ConcreteComponent x0 = ConcreteComponent("foo");
-  ConcreteDecoratorB x1 = ConcreteDecoratorB(new ConcreteComponent("foo"));
-  ConcreteDecoratorA x2 = ConcreteDecoratorA(new ConcreteDecoratorB(new ConcreteComponent("foo")));
-  ConcreteDecoratorB x3 = ConcreteDecoratorB(new ConcreteDecoratorA(new ConcreteDecoratorB(new ConcreteComponent("foo"))));
-  x0.Display();
-  x1.Display();
-  x2.Display();
-  x3.Display();
+  Component* x0 = new ConcreteComponent("foo");
+  Component* x1 = new ConcreteDecoratorB(new ConcreteComponent("foo"));
+  Component* x2 = new ConcreteDecoratorA(new ConcreteDecoratorB(new ConcreteComponent("foo")));
+  Component* x3 = new ConcreteDecoratorB(new ConcreteDecoratorA(new ConcreteDecoratorB(new ConcreteComponent("foo"))));
+  x0->Display();
+  x1->Display();
+  x2->Display();
+  x3->Display();
   std::cout << "==== end ====" << std::endl;
 }
