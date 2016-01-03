85c0de36ad7796883966f446c8e62b27f0dd1bcf
inazo <inaba1115@gmail.com>
2016-01-03 19:18:39 +0900

    Add lcm

--- math/gcd_lcm.cpp
+++ math/gcd_lcm.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-/* euclidean_algorithm */
+/* euclidean_algorithm , gcm */
 int gcd(int x, int y) {
   int rem = x % y;
   if(rem == 0) {
@@ -10,7 +10,13 @@ int gcd(int x, int y) {
   }
 }
 
+int lcm(int x, int y) {
+  return x * y / gcd(x, y);
+}
+
 int main() {
   std::cout << gcd(9, 6) << std::endl;
+  std::cout << lcm(9, 6) << std::endl;
   std::cout << gcd(1071, 1029) << std::endl;
+  std::cout << lcm(1071, 1029) << std::endl;
 }
