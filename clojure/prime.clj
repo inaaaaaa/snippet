2b095dd52ab33864b61307a733c2aecbf37296b8
inazo <inaba1115@gmail.com>
2016-03-20 14:51:20 +0900

    Fix

--- clojure/prime.clj
+++ clojure/prime.clj
@@ -1,7 +1,7 @@
 (defn prime [lst]
   (if (empty? lst) '()
       (cons (first lst)
-            (prime (filter (fn [x] (not (= (mod x (first lst)) 0)))
+            (prime (filter (fn [x] (not (zero? (mod x (first lst)))))
                            (rest lst))))))
 
 ;; main
