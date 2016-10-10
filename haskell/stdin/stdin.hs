c1973c19b0bf36354996cf54ac116e7ea94c18d5
inazo1115 <inaba1115@gmail.com>
2016-10-10 23:02:04 +0900

    Update

--- haskell/stdin/stdin.hs
+++ haskell/stdin/stdin.hs
@@ -3,13 +3,15 @@ import Control.Monad
 
 {-
   $ cat input.txt
-  1 2 3
-  4 5 6
-  7 8 9
+  0 1 2
+  3 4 5
+  6 7 8
+  9 0 1
   $ cat input.txt | stack runhaskell stdin.hs
-  [1,2,3]
-  [4,5,6]
-  [7,8,9]
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  [9,0,1]
 -}
 
 main = do
@@ -19,8 +21,12 @@ main = do
   print a'
 
   -- 1
-  b <- map (read :: String -> Int) . words <$> getLine
+  b <- fmap ((fmap (read :: String -> Int)) . words) getLine
   print b
 
   -- 2
+  c <- map (read :: String -> Int) . words <$> getLine
+  print c
+
+  -- 3
   map (read :: String -> Int) . words <$> getLine >>= print
