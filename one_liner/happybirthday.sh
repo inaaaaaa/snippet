362aa629f1dc099fde54d170e7782d980efa5ec7
inazo1115 <inaba1115@gmail.com>
2017-02-10 11:40:21 +0900

    Organize dir

--- one_liner/happybirthday.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# This one-liner only could run on macosx. Maybe.
-
-while true; do banner -w 100 HappyBirthday | sed -e 's/ /_/g' | while read l; do printf $l; for i in $(seq `echo $l | wc -c | xargs -I{} echo '100 - ' {} | bc`); do printf "_"; done; sleep 0.01; echo ""; done; done
